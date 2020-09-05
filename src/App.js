f369085b4e6b5e728491297c6c462df50abc9ab9
Andres Medrano <amedra13@gmail.com>
2020-09-05 01:13:57 -0700

    create carousel file and add include new pictures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Carousel from './components/Carousel';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<h1>Hello Everyone</h1>
+			<Carousel />
 		</div>
 	);
 }
