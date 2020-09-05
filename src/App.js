5cb5ed07ac838e677da35a90aabcaa37878f8602
Andres Medrano <amedra13@gmail.com>
2020-09-05 01:50:00 -0700

    added description componenet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Carousel from './components/Carousel';
+import Description from './components/Description';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Carousel />
+			<Description />
 		</div>
 	);
 }
---
+++ src/components/Description.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './description.css';
+
+const Description = () => {
+	return (
+		<div className="Description">
+			<h6>NOBU HOTEL IN SILICON VALLEY</h6>
+			<h3>DOWNTOWN PALO ALTO</h3>
+			<p>WHERE MODERN JAPANESE DESIGN MEETS WEST COAST LUXURY LIFESTYLE</p>
+			<br />
+			<p>
+				​Our 73-room boutique hotel in the heart of Silicon Valley is undergoing
+				an exciting transformation to elevate your Nobu experience to new
+				heights, starting the moment you check-in. You will arrive by way of a
+				grand spiral staircase with floating stone treads and a carved teak
+				handrail, set above the globally acclaimed Nobu Restaurant.
+			</p>
+			<br />
+			<p>
+				Then you will be welcomed into a light-filled lounge with rich wood,
+				Crema Marfil stone, Venetian plaster ceilings, and luxurious furnishings
+				like George Nakashima’s legendary chairs and John Wigmore’s ethereal
+				light sculptures.
+			</p>
+			<br />
+			<p>
+				Designed by LA-based Montalba Architects, the Hotel will also feature a
+				new façade of chiseled stone, textured with waves of cream and gold. The
+				peaceful and buoyant design will express subtle echoes and contrasts
+				with the hotel’s commanding views of the city, Stanford University, the
+				Santa Cruz Mountains, and the surrounding Bay Area foothills.
+			</p>
+			<br />
+			<p>Enjoy world-renowned Nobu hospitality, Palo Alto-style.</p>
+			<br />
+		</div>
+	);
+};
+
+export default Description;
